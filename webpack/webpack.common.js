6b9ab8039e60b0261e8b01b31cbdfe6f8c50afb8
장한울 <swingjhw@naver.com>
2023-06-23 17:02:34 +0900

    feat: webpack 설정

---
+++ webpack/webpack.common.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+    entry: {
+      popup: path.join(srcDir, 'popup.ts'),
+      background: path.join(srcDir, 'background.ts'),
+    },
+    output: {
+        path: path.join(__dirname, "../dist/js"),
+        filename: "[name].js",
+    },
+    optimization: {
+        splitChunks: {
+            name: "vendor",
+            chunks(chunk) {
+              return chunk.name !== 'background';
+            }
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test:/\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: "../", context: "public" }],
+            options: {},
+        }),
+    ],
+};
