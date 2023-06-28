1c1c241769116af788d2e7d182e59e08772ae828
장한울 <swingjhw@naver.com>
2023-06-28 16:34:48 +0900

    feat(getElement): 엘리먼트 받는 함수  $, 41080, getElById 생성

---
+++ src/utils/getElement.ts
@@ -0,0 +1,18 @@
+
+type $ = (targetName:string, parentEl?: HTMLElement) => HTMLElement | null
+export const $:$ = (targetName, parentEl) => {
+    if(parentEl) return parentEl.querySelector(targetName)
+    return document.querySelector(targetName)
+}
+
+type $$ = (targetName:string, parentEl?:HTMLElement) => NodeListOf<Element> | null
+export const $$:$$ = (targetName, parentEl) => {
+    if(parentEl) return parentEl.querySelectorAll(targetName)
+    return document.querySelectorAll(targetName)
+}
+
+type getElById = (targetId:string) => HTMLElement | null 
+export const getElById:getElById = (targetId) => {
+    return document.getElementById(targetId)
+}
+
