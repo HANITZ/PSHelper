624092f9fd593b4e1769bcb7925da51e3d5681e2
장한울 <swingjhw@naver.com>
2023-07-02 17:16:18 +0900

    feat(webpack.common.js): html-loader 추가

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -31,6 +31,10 @@ module.exports = {
                 test:/\.css$/,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
         ],
     },
     resolve: {
