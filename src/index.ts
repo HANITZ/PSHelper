0cdf89d69c75d87bc3420fea8204ec24c0b09364
장한울 <swingjhw@naver.com>
2023-07-19 19:45:29 +0900

    feat: .env 설정해서 github client id, secret 숨김

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-const url = new URL(window.location.href);
-const githubCode = url.searchParams.get("code");
-console.log(githubCode);
 
+async function Git() {
+  const url = new URL(window.location.href);
+  const githubCode = url.searchParams.get("code");
+
+  if (!githubCode) throw new Error("GITGUB인증실패");
+  const getAccessToken = async (code: string): Promise<any> => {
+    const res = await (
+      await fetch("https://github.com/login/oauth/access_token", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      })
+    ).json();
+
+    return res;
+  };
+
+  const res = await getAccessToken(githubCode);
+  console.log(res.access_token);
+}
+
+Git();
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: {
@@ -13,6 +14,9 @@ module.exports = {
     path: path.join(__dirname, "../dist/js"),
     filename: "[name].js",
   },
+  resolve: {
+    extensions: [ ".js"],
+  },
   optimization: {
     splitChunks: {
       name: "vendor",
@@ -23,6 +27,21 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.ts$|tsx/,
+        include: [path.resolve(__dirname, './src')],
+        exclude: ["/node_modules"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties"],
+              ["@babel/plugin-transform-runtime", { corejs: 3 }],
+            ],
+          },
+        },
+      },
       {
         test: /\.tsx?$/,
         use: "ts-loader",
@@ -42,6 +61,7 @@ module.exports = {
     extensions: [".ts", ".js"],
   },
   plugins: [
+    new Dotenv(),
     new CopyPlugin({
       patterns: [{ from: ".", to: "../", context: "public" }],
       options: {},
