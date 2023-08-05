35dbc94f2a9f1d346b0354305b95bf9acb5a8ec4
장한울 <swingjhw@naver.com>
2023-08-05 18:49:56 +0900

    feat(Github): 에러 설정 , Github 로그인 성공 시 true반환하도록 변경

--- src/Github.ts
+++ src/Github.ts
@@ -1,25 +1,26 @@
 import { sendChromeMessage } from "./chromeUtils";
 import { getAccessToken } from "./getReqAPI";
 
-async function Github() {
+export async function Github() {
   const url = new URL(window.location.href);
   const githubCode = url.searchParams.get("code");
 
-  if (!githubCode) throw new Error("GITHUB인증실패");
-
-  const res = await getAccessToken(githubCode);
-  if (res) {
-    sendChromeMessage({
-      closeWebPage: true,
-      isSuccess: true,
-      token: res.access_token,
-      action: "PSHELPER_TOKEN",
-    });
+  if (!githubCode) throw new Error("Failed Get GITHUB Authorization Code ");
+  try {
+    const res = await getAccessToken(githubCode);
+    if (res) {
+      sendChromeMessage({
+        closeWebPage: true,
+        isSuccess: true,
+        token: res.access_token,
+        action: "PSHELPER_TOKEN",
+      });
+      if (window.location.host === "github.com") {
+        window.close();
+      }
+      return true;
+    }
+  } catch (error) {
+    throw error;
   }
-  
-  window.close();
-}
-
-if (window.location.host === "github.com") {
-  Github();
 }
