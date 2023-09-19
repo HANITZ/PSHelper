d657c07a7728aaddfe1ac9a42a39a7356c823dfe
장한울 <swingjhw@naver.com>
2023-09-19 22:49:49 +0900

    feat(Baekjun): afterSubmit 수정

--- src/Baekjun.ts
+++ src/Baekjun.ts
@@ -5,6 +5,7 @@ import {
   enrollEvent,
   getTimeDiff,
   getTimeNow,
+  hasElement,
   isObjEmpty,
 } from "./utils/jsUtils";
 import "./Baekjun.css";
@@ -32,87 +33,6 @@ class Baekjun {
     this.init();
     this.checkSubmited();
   }
-  tmp = () => {
-    // const table = $("#status-table") as HTMLTableElement;
-    // function convertResultTableHeader(header: string) {
-    //   switch (header) {
-    //     case "문제번호":
-    //     case "문제":
-    //       return "problemId";
-    //     case "난이도":
-    //       return "level";
-    //     case "결과":
-    //       return "result";
-    //     case "문제내용":
-    //       return "problemDescription";
-    //     case "언어":
-    //       return "language";
-    //     case "제출 번호":
-    //       return "submissionId";
-    //     case "아이디":
-    //       return "username";
-    //     case "제출시간":
-    //     case "제출한 시간":
-    //       return "submissionTime";
-    //     case "시간":
-    //       return "runtime";
-    //     case "메모리":
-    //       return "memory";
-    //     case "코드 길이":
-    //       return "codeLength";
-    //     default:
-    //       return "unknown";
-    //   }
-    // }
-
-    // const headers = Array.from(table.rows[0].cells, (x) =>
-    //   convertResultTableHeader(x.innerText.trim())
-    // );
-    const table = $("#status-table") as HTMLTableElement;
-    const isCorrect = Array.from(table.rows[1].cells)[3];
-
-    // const list = [];
-    // for (let i = 1; i < table.rows.length; i++) {
-    //   const row = table.rows[i];
-    //   const cells = Array.from(row.cells, (x, index) => {
-    //     switch (headers[index]) {
-    //       case 'result':
-    //         return { result: x.innerText.trim(), resultCategory: x.firstChild.getAttribute('data-color').replace('-eng', '').trim() };
-    //       case 'language':
-    //         return x.innerText.unescapeHtml().replace(/\/.*$/g, '').trim();
-    //       case 'submissionTime':
-    //         const el = x.querySelector('a.show-date');
-    //         if (isNull(el)) return null;
-    //         return el.getAttribute('data-original-title');
-    //       case 'problemId':
-    //         const a = x.querySelector('a.problem_title');
-    //         if (isNull(a)) return null;
-    //         return {
-    //           problemId: a.getAttribute('href').replace(/^.*\/([0-9]+)$/, '$1'),
-    //         };
-    //       default:
-    //         return x.innerText.trim();
-    //     }
-    //   });
-    //   let obj = {};
-    //   obj.elementId = row.id;
-    //   for (let j = 0; j < headers.length; j++) {
-    //     obj[headers[j]] = cells[j];
-    //   }
-    //   obj = { ...obj, ...obj.result, ...obj.problemId};
-    //   list.push(obj);
-    // }
-    // log('TableList', list);
-    // return list;
-
-    const textAreaElement = $(".CodeMirror-code");
-
-    setTimeout(() => {
-      const divs = $$(".CodeMirror-line > span > span", textAreaElement);
-      const a = divs.map((div) => div.innerHTML);
-      // .reduce((acc, div) => acc + div, "");
-    }, 5000);
-  };
 
   getProblemNumber = (): string => {
     const problemId = $(
@@ -141,24 +61,29 @@ class Baekjun {
   afterSubmit = async () => {
     this.startLoading();
     const checkSuccessInterval = setInterval(() => {
-      const resultEl = $(".result-ac");
-      if (resultEl) {
+      const statusTable = $("#status-table") as HTMLTableElement;
+      const isCorrectElement = statusTable.rows[1].cells[3] as HTMLElement;
+      if (hasElement(".result-ac", isCorrectElement)) {
         if (this.checkSuccess()) {
-          console.log("성공");
+          clearInterval(checkSuccessInterval);
           this.afterSuccess();
+          return;
         }
+
+        clearInterval(checkSuccessInterval);
         this.afterFail();
       }
     }, 2000);
   };
 
   afterSuccess = () => {
-    this.renderFailMark();
+    this.renderSuccessMark();
+    this.getSolvedData();
+    const categories = $$(".CodeMirror-line > span > span");
   };
 
   afterFail = () => {
-    this.renderSuccessMark();
-    this.getSolvedData();
+    this.renderFailMark();
   };
 
   getSolvedData = () => {};
@@ -179,8 +104,6 @@ class Baekjun {
   };
 
   init = async () => {
-    this.tmp();
-
     const { BaekjunProblemId: baekjunProblemId } = (await getChromeLocalStorage(
       "BaekjunProblemId"
     )) as BaekjunProblemId;
@@ -273,5 +196,4 @@ if (window.location.href.includes("/problem/")) {
 } else if (window.location.href.includes("/submit/")) {
   baekjun.readyToSubmit();
 }
-baekjun.afterSubmit();
 console.log("백준실행");
