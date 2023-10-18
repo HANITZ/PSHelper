05bcd10178a0b3b048c1ae83c9e26bd6368ecc47
장한울 <swingjhw@naver.com>
2023-10-18 22:27:23 +0900

    feat(Component): setState에서 존재하지 않는 키이거나 변경하려는 상태가 기존과 동일하면 리렌더링 막도록 설정

--- src/view/Home/Component.ts
+++ src/view/Home/Component.ts
@@ -2,38 +2,47 @@ import { addComponents } from "../../utils/jsUtils";
 
 export type ComponentProps<T> = {
   node: Element;
-  state: T | null;
+  state: T;
 };
 
 export default abstract class Component<T> {
   node: Element;
-  #state: T | {};
+  state: T;
   constructor({ node, state }: ComponentProps<T>) {
     this.node = node;
-    this.#state = state ? state : {};
+    this.state = state;
     this.render();
   }
 
-  setState(newState: object) {
-    this.#state = {
-      ...this.#state,
+  setState(newState: Partial<T>) {
+    for (const key in newState) {
+      if (!Object.prototype.hasOwnProperty.call(newState, key)) {
+        throw new Error(`${key}는 상태에 존재하지 않는 변수입니다.`)
+      }
+      if(this.state[key] === newState[key]){
+        return 
+      }
+    }
+    this.state = {
+      ...this.state,
       ...newState,
     };
     this.render();
   }
 
   render(): void {
-    addComponents.call(this)
-    this.createChildComponents()
+    addComponents.call(this);
+    this.createChildComponents();
+    this.setEvent();
   }
 
-  createChildComponents():void{}
-  
+  async createChildComponents(): Promise<void> {}
+
   update(): void {}
 
   template(): string {
     return "";
   }
 
-  init(): void {}
+  setEvent(): void {}
 }
