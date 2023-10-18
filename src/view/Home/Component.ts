12b2b30bf160098e6e76ea12ddc9c0930dc2a564
장한울 <swingjhw@naver.com>
2023-10-19 05:08:14 +0900

    feat(Component): setState에서 checkChangeState 함수 분리

--- src/view/Home/Component.ts
+++ src/view/Home/Component.ts
@@ -14,20 +14,28 @@ export default abstract class Component<T> {
     this.render();
   }
 
-  setState(newState: Partial<T>) {
-    for (const key in newState) {
-      if (!Object.prototype.hasOwnProperty.call(newState, key)) {
-        throw new Error(`${key}는 상태에 존재하지 않는 변수입니다.`)
-      }
-      if(this.state[key] === newState[key]){
-        return 
-      }
+  setState(newState: Partial<T>):void {
+    if(!this.checkChangeState(newState)){
+      return
     }
+
     this.state = {
       ...this.state,
       ...newState,
     };
     this.render();
+  }
+  
+  checkChangeState(newState: Partial<T>):boolean{
+    for (const key in newState) {
+      if (!Object.prototype.hasOwnProperty.call(newState, key)) {
+        throw new Error(`${key}는 상태에 존재하지 않는 변수입니다.`)
+      }
+      if(this.state[key] === newState[key]){
+        return false
+      }
+    }
+    return true
   }
 
   render(): void {
