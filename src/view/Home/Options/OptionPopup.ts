72757b40b81c1bba1cb7c6a8c45a58458b138973
장한울 <swingjhw@naver.com>
2023-10-22 15:00:44 +0900

    feat(Options): option 구현

---
+++ src/view/Home/Options/OptionPopup.ts
@@ -0,0 +1,64 @@
+import { Timer, Upload } from "../../../Popup/Popup";
+import {
+  getChromeLocalStorage,
+  sendChromeMessage,
+} from "../../../utils/chromeUtils";
+import { isUndefined, selectEl } from "../../../utils/jsUtils";
+import Component from "../Component";
+import OptionSlide from "./OptionSlide";
+
+type PropsOptionPopup = {
+  Timer?: boolean;
+  Upload?: boolean;
+};
+
+export default class OptionPopup extends Component<PropsOptionPopup> {
+  createChildComponents() {
+    const { Timer, Upload } = this.state;
+
+    new OptionSlide({
+      node: selectEl("OptionIsUpload", this.node),
+      state: {
+        id: "Upload",
+        isActive: Upload,
+        optionTitle: "Uploading code to Repo",
+      },
+    });
+
+    new OptionSlide({
+      node: selectEl("OptionIsTimer", this.node),
+      state: {
+        id: "Timer",
+        isActive: Timer,
+        optionTitle: "Auto Timer",
+      },
+    });
+  }
+  async componentDidMount() {
+    let { Timer } = (await getChromeLocalStorage("Timer")) as Timer;
+    let { Upload } = (await getChromeLocalStorage("Upload")) as Upload;
+
+    if (isUndefined(Timer)) {
+      Timer = true;
+      sendChromeMessage({ action: "Timer", method: "post", isActive: Timer });
+    }
+    if (isUndefined(Upload)) {
+      Upload = true;
+      sendChromeMessage({ action: "Upload", method: "post", isActive: Upload });
+    }
+    this.setState({ Timer, Upload });
+  }
+  template(): string {
+    return `
+    <div class="option-container" >
+      <div class="option-header">
+        <h1 class="option-title">Option</h1>
+      </div>
+      <div class="option-body">
+        <OptionIsUpload class="option isUpload" ></OptionIsUpload>
+        <OptionIsTimer class="option isTimer" ></OptionIsTimer>
+      </div>
+    </div>
+    `;
+  }
+}
---
+++ src/view/Home/Options/OptionSlide.ts
@@ -0,0 +1,33 @@
+import { sendChromeMessage } from "../../../utils/chromeUtils";
+import { selectEl } from "../../../utils/jsUtils";
+import Component from "../Component";
+
+type PropsOption = {
+  id: string;
+  isActive?: boolean;
+  optionTitle: string;
+};
+
+export default class OptionSlide extends Component<PropsOption> {
+  setEvent() {
+    const { id } = this.state;
+    selectEl(".slider", this.node).addEventListener("click", (e) => {
+      e.preventDefault();
+      const input = selectEl("input", this.node) as HTMLInputElement;
+      sendChromeMessage({ action: id, method: "post",  isActive: !input.checked });
+      input.checked = !input.checked;
+    });
+  }
+  template() {
+    const { id, isActive, optionTitle } = this.state;
+    return `
+    <div>
+      <span>${optionTitle}</span>
+      <label for="${id}" class="repo-switch">
+        <input type="checkbox" id="${id}" ${isActive ? "checked" : ""} />
+        <span class="slider"></span>
+      </label>
+    </div>
+    `;
+  }
+}
